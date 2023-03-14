cd766120d42e712063fe934d69ddb86c516a7734
Pavlo Myroniuk <the.best.tvarynka@gmail.com>
2023-03-14 17:41:39 +0200

    feat(docker-rust-examples): add simple Dockerfile

--- docker-rust-examples/src/main.rs
+++ docker-rust-examples/src/main.rs
@@ -50,7 +50,7 @@ async fn main() -> std::io::Result<()> {
             .service(db_status)
             .route("/hey", web::get().to(manual_hello))
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
