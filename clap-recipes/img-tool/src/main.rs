db24a5bb5476d05eff35222955354cca7ef83797
Pavlo Myroniuk <the.best.tvarynka@gmail.com>
2023-06-14 00:09:56 +0300

    feat(clap-recipes): implement the second recipe;

---
+++ clap-recipes/img-tool/src/main.rs
@@ -0,0 +1,60 @@
+use std::path::PathBuf;
+
+use clap::{command, ArgGroup, Args, Parser, Subcommand};
+use url::Url;
+
+#[derive(Debug, Clone, Args)]
+// #[command(subcommand)]
+#[clap(group(
+    ArgGroup::new("file-source")
+        .required(true)
+        .args(&["file", "link"]),
+))]
+/// Possible types of the file source
+struct FileSource {
+    /// Path to the image on the device
+    #[arg(long)]
+    file: Option<PathBuf>,
+
+    /// Url to the image on the Internet
+    #[arg(long)]
+    link: Option<Url>,
+}
+
+#[derive(Debug, Clone, Subcommand)]
+enum Command {
+    Upload {
+        /// File to upload
+        #[command(flatten)]
+        file_source: FileSource,
+
+        /// Folder for the image on the site
+        #[arg(long)]
+        folder: String,
+    },
+    Download {
+        /// Source image link
+        #[arg(long)]
+        link: Url,
+
+        /// Path for the image
+        #[arg(long)]
+        dest_file: PathBuf,
+    },
+}
+
+/// Img tool config structure
+#[derive(Parser, Debug)]
+struct Config {
+    /// command to execute
+    #[command(subcommand)]
+    pub command: Command,
+
+    /// Path to the api key file
+    #[arg(long, env = "API-KEY")]
+    pub api_key: PathBuf,
+}
+
+fn main() {
+    println!("{:?}", Config::parse());
+}
