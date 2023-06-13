eae20b208693f6dafeb7910fd8b26ca24987937b
Pavlo Myroniuk <the.best.tvarynka@gmail.com>
2023-06-13 22:26:29 +0300

    feat(clap-recipes): values-collection: improve server configuration;

--- clap-recipes/values-collection/src/cipher.rs
+++ clap-recipes/values-collection/src/cipher.rs
@@ -1,10 +1,5 @@
 use std::fmt::{self, Display, Formatter};
 
-use clap::{
-    error::{ContextKind, ContextValue, ErrorKind},
-    Error,
-};
-
 const AES128: &str = "aes128";
 const AES256: &str = "aes256";
 const DES3: &str = "des3";
@@ -29,29 +24,5 @@ impl AsRef<str> for Cipher {
 impl Display for Cipher {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         f.write_str(self.as_ref())
-    }
-}
-
-impl TryFrom<&str> for Cipher {
-    type Error = Error;
-
-    fn try_from(value: &str) -> Result<Self, Self::Error> {
-        match value {
-            AES128 => Ok(Self::Aes128),
-            AES256 => Ok(Self::Aes256),
-            DES3 => Ok(Self::Des3),
-            _ => {
-                let mut error = Error::new(ErrorKind::InvalidValue);
-                error.insert(
-                    ContextKind::InvalidArg,
-                    ContextValue::String("enc-algs".into()),
-                );
-                error.insert(
-                    ContextKind::InvalidValue,
-                    ContextValue::String(format!("Invalid algorithm name: {}", value)),
-                );
-                Err(error)
-            }
-        }
     }
 }
