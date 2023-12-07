5be01fe7729e007af98cfb41a8f600ea76197eb5
Pavlo Myroniuk <the.best.tvarynka@gmail.com>
2023-12-07 16:43:20 +0000

    rust-hyte: one more clippy example

--- what-the-hype-is-all-about/src/cool_part/smart_compiler.rs
+++ what-the-hype-is-all-about/src/cool_part/smart_compiler.rs
@@ -15,3 +15,21 @@
 fn in_range(i: usize) -> bool {
     i >= 10 && i < 20
 }
+
+// warning: match expression looks like `matches!` macro
+//   --> src/asn1/hex_view.rs:52:5
+//    |
+// 52 | /     match cur_node {
+// 53 | |         Some(node_id) if *node_id == asn1_node_id => true,
+// 54 | |         _ => false,
+// 55 | |     }
+//    | |_____^ help: try: `matches!(cur_node, Some(node_id) if *node_id == asn1_node_id)`
+//    |
+//    = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#match_like_matches_macro
+//    = note: `#[warn(clippy::match_like_matches_macro)]` on by default
+fn compare_ids(asn1_node_id: u64, cur_node: &Option<u64>) -> bool {
+    match cur_node {
+        Some(node_id) if *node_id == asn1_node_id => true,
+        _ => false,
+    }
+}
