10c5887226421b1fcf58cda968bda77968f09f28
Pavlo Myroniuk <the.best.tvarynka@gmail.com>
2024-06-13 00:16:14 +0300

    feat(git): tbt-app: add conversion string

--- git-lection/tbt-app/src/main.rs
+++ git-lection/tbt-app/src/main.rs
@@ -1,3 +1,15 @@
+use std::slice::from_raw_parts;
+
+pub unsafe fn c_w_str_to_string(s: *const u16) -> String {
+    let mut len = 0;
+
+    while unsafe { *(s.add(len)) } != 0 {
+        len += 1;
+    }
+
+    String::from_utf16_lossy(unsafe { from_raw_parts(s, len) })
+}
+
 fn main() {
     println!("Tbt was here.");
 }
