45070e03c6b784dc3e2c5f1df43d98643d8b90e6
Pavlo Myroniuk <the.best.tvarynka@gmail.com>
2024-08-28 15:06:22 +0300

    reverse_freerdp_scard_auth: wip

--- reverse_freerdp_scard_auth/cmds.sh
+++ reverse_freerdp_scard_auth/cmds.sh
@@ -18,8 +18,22 @@ yubico-piv-tool -a import-key -a import-certificate -s 9c -k -i ~/Downloads/Tele
 pkcs11-tool --module "/usr/local/lib/libykcs11.so.2.5.2" --show-info -v
 pkcs11-tool --module "/usr/local/lib/libykcs11.so.2.5.2" -L -l -p 123456 -O
 cat data | pkcs11-tool --module "/usr/local/lib/libykcs11.so.2.5.2" -m RSA-PKCS -p 123456 -s --id 02
-at delete_it | LD_LIBRARY_PATH=/usr/local/lib pkcs11-tool --module "/usr/local/lib/libykcs11.so.2.5.2" -m RSA-PKCS -p 123456 -s --id 02 | base64
+cat delete_it | LD_LIBRARY_PATH=/usr/local/lib pkcs11-tool --module "/usr/local/lib/libykcs11.so.2.5.2" -m RSA-PKCS -p 123456 -s --id 02 | base64
 LD_LIBRARY_PATH=/usr/local/lib 
 gcc main.c -I/usr/include/pkcs11-helper-1.0 && LD_LIBRARY_PATH=/usr/local/lib ./a.out
 LD_LIBRARY_PATH=/usr/local/lib pkcs11-tool --module "/usr/local/lib/libykcs11.so.2.5.2" --list-token-slots
-cmake -GNinja -B freerdp-build-6 -S ./ -DCMAKE_BUILD_TYPE=Debug -DCMAKE_SKIP_INSTALL_ALL_DEPENDENCY=ON -DWITH_SERVER=OFF -DWITH_SAMPLE=OFF -DWITH_PLATFORM_SERVER=OFF -DUSE_UNWIND=OFF -DWITH_SWSCALE=OFF -DWITH_FFMPEG=OFF -DWITH_WEBVIEW=OFF -WITH_PKCS11=ON -WITH_KRB5=ON -WITH_KRB5_NO_NTLM_FALLBACK=ON -DCHANNEL_URBDRC=OFF -DCMAKE_INSTALL_PREFIX=/usr/local -WITH_SMARTCARD_PCSC=ON
+
+/usr/local/lib/libwinpr3.so
+LD_PRELOAD=/usr/lib/libpcsclite.so.1 LD_LIBRARY_PATH=/usr/local/lib xfreerdp /list:smartcard:pkcs11-module:/usr/local/lib/libykcs11.so.2.5.2
+cat delete_it | pkcs11-tool --module "libykcs11.so.2.5.2" -m RSA-PKCS -p 123456 -s --id 02 | base64
+
+LD_PRELOAD=/usr/lib/libpcsclite.so.1 xfreerdp /v:DESKTOP-8F33RFH.tbt.com /u:t2@tbt.com /p:123456 /cert:ignore /log-level:TRACE /smartcard-logon /sec:nla
+LD_PRELOAD=/usr/lib/libpcsclite.so.1 xfreerdp /v:DESKTOP-8F33RFH.tbt.com /u:t2@tbt.com /p:123456 /cert:ignore /log-level:TRACE /smartcard-logon /sec:nla /kerberos:pkcs11-module:libykcs11.so.2.5.2 /auth-pkg-list:\!ntlm,kerberos
+
+WITH_KRB5_MIT
+
+export KRB5_TRACE=/dev/stdout
+export KRB5_CONFIG=/home/pavlo-myroniuk/apriorit/reverse_freerdp_scard_auth/krb5.conf
+export WLOG_LEVEL=trace
+
+LD_PRELOAD=/usr/lib/libpcsclite.so.1 gdb --args xfreerdp /v:DESKTOP-8F33RFH.tbt.com /u:t2@tbt.com /p:123456 /cert:ignore /log-level:TRACE /smartcard-logon /sec:nla /kerberos:pkcs11-module:libykcs11.so.2.5.2 /auth-pkg-list:\!ntlm,kerberos /winscard-module:libsspi.so
