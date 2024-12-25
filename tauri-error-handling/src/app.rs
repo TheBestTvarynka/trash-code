265f39bfde2aa9fb10055bfee5031714498f7b28
Pavlo Myroniuk <the.best.tvarynka@gmail.com>
2024-12-25 13:40:01 +0200

    feat(tauri-error-handling): init tauri app;

---
+++ tauri-error-handling/src/app.rs
@@ -0,0 +1,40 @@
+use leptos::task::spawn_local;
+use leptos::{ev::SubmitEvent, prelude::*};
+
+use crate::backend::greet;
+
+#[component]
+pub fn App() -> impl IntoView {
+    let (name, set_name) = signal(String::new());
+    let (greet_msg, set_greet_msg) = signal(String::new());
+
+    let update_name = move |ev| {
+        let v = event_target_value(&ev);
+        set_name.set(v);
+    };
+
+    let greet = move |ev: SubmitEvent| {
+        ev.prevent_default();
+        spawn_local(async move {
+            let name = name.get_untracked();
+            if name.is_empty() {
+                return;
+            }
+            
+            set_greet_msg.set(greet(&name).await);
+        });
+    };
+
+    view! {
+        <main class="container">
+            <form class="row" on:submit=greet>
+                <input
+                    id="greet-input"
+                    on:input=update_name
+                />
+                <button type="submit">"Greet"</button>
+            </form>
+            <p>{ move || greet_msg.get() }</p>
+        </main>
+    }
+}
---
+++ tauri-error-handling/src/backend.rs
@@ -0,0 +1,20 @@
+use serde::{Deserialize, Serialize};
+use serde_wasm_bindgen::to_value;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = ["window", "__TAURI__", "core"])]
+    async fn invoke(cmd: &str, args: JsValue) -> JsValue;
+}
+
+#[derive(Serialize, Deserialize)]
+struct GreetArgs<'a> {
+    name: &'a str,
+}
+
+pub async fn greet(name: &str) -> String {
+    let args = to_value(&GreetArgs { name }).unwrap();
+    
+    invoke("greet", args).await.as_string().unwrap()
+}
